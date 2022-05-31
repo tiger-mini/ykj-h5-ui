e69cb8315682366deafd326e832c7aed43c9c412
yangb03 <yangb03@mingyuanyun.com>
2022-05-31 14:33:00 +0800

    feat: deploy docs

--- examples/App.vue
+++ examples/App.vue
@@ -4,12 +4,16 @@
     <div class='main-wrapper'>
       <div class="ant-row" style="row-gap: 0px;">
         <nav-side :data='navsData'></nav-side>
-        <div class='ant-col ant-col-xs-24 ant-col-sm-24 ant-col-md-18 ant-col-lg-18 ant-col-xl-19 ant-col-xxl-20'>
+        <div class='ant-col'>
+<!--          ant-col-xs-24 ant-col-sm-24 ant-col-md-18 ant-col-lg-18 ant-col-xl-19 ant-col-xxl-20-->
           <section class='main-container'>
             <section class='class="markdown"'>
               <router-view></router-view>
             </section>
           </section>
+        </div>
+        <div class='simulator-wrap'>
+          <simulater />
         </div>
       </div>
     </div>
@@ -19,9 +23,11 @@
 import Logo from './components/Logo'
 import navSide from './components/menuCom'
 import navsData from './router/routerCon.json'
+import Simulater from './components/simulater'
 
 export default {
   components: {
+    Simulater,
     Logo,
     navSide
   },
@@ -114,12 +120,31 @@ body {
   border-right: #d3dce6 3px solid;
 }
 
+.demo-home-nav__title {
+  margin: 24px 0 8px 16px;
+  color: rgba(69, 90, 100, 0.6);
+  font-size: 14px;
+}
+
 .view {
   padding: 0 0 80px 0;
   float: left;
   height: calc(100% - 50px);
   width: calc(100% - 203px);
   overflow: auto;
+}
+.simulator-wrap {
+  position: fixed;
+  right: 60px;
+  top: 100px;
+
+  box-sizing: border-box;
+  width: 360px;
+  min-width: 360px;
+  overflow: hidden;
+  background: #fafafa;
+  border-radius: 12px;
+  box-shadow: #ebedf0 0 4px 12px;
 }
 </style>
 <style lang='scss' scoped>
---
+++ examples/components/simulater.vue
@@ -0,0 +1,295 @@
+<template>
+  <div class='simulator'>
+    <header>
+      <span> 返回 </span>
+      <span>头部</span>
+    </header>
+    <section>
+      <h3 class='demo-home__title'><img src='https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg'><span>YKJ H5 UI Design</span>
+      </h3>
+      <h2 class='demo-home__desc'>
+        轻量、可靠的移动端组件库
+      </h2>
+
+      <div class='demo-home-nav'>
+        <div class='demo-home-nav__title'>基础组件</div>
+        <div class='demo-home-nav__group'><a href='#/zh-CN/button' class='demo-home-nav__block'>
+          Button 按钮
+          <svg viewBox='0 0 1024 1024' class='demo-home-nav__icon'>
+            <path fill='#B6C3D2'
+                  d='M601.1 556.5L333.8 289.3c-24.5-24.5-24.5-64.6 0-89.1s64.6-24.5 89.1 0l267.3 267.3c24.5 24.5 24.5 64.6 0 89.1-24.5 24.4-64.6 24.4-89.1-.1z'></path>
+            <path fill='#B6C3D2'
+                  d='M690.2 556.5L422.9 823.8c-24.5 24.5-64.6 24.5-89.1 0s-24.5-64.6 0-89.1l267.3-267.3c24.5-24.5 64.6-24.5 89.1 0 24.5 24.6 24.5 64.6 0 89.1z'></path>
+          </svg>
+        </a><a href='#/zh-CN/cell' class='demo-home-nav__block'>
+          Cell 单元格
+          <svg viewBox='0 0 1024 1024' class='demo-home-nav__icon'>
+            <path fill='#B6C3D2'
+                  d='M601.1 556.5L333.8 289.3c-24.5-24.5-24.5-64.6 0-89.1s64.6-24.5 89.1 0l267.3 267.3c24.5 24.5 24.5 64.6 0 89.1-24.5 24.4-64.6 24.4-89.1-.1z'></path>
+            <path fill='#B6C3D2'
+                  d='M690.2 556.5L422.9 823.8c-24.5 24.5-64.6 24.5-89.1 0s-24.5-64.6 0-89.1l267.3-267.3c24.5-24.5 64.6-24.5 89.1 0 24.5 24.6 24.5 64.6 0 89.1z'></path>
+          </svg>
+        </a><a href='#/zh-CN/icon' class='demo-home-nav__block'>
+          Icon 图标
+          <svg viewBox='0 0 1024 1024' class='demo-home-nav__icon'>
+            <path fill='#B6C3D2'
+                  d='M601.1 556.5L333.8 289.3c-24.5-24.5-24.5-64.6 0-89.1s64.6-24.5 89.1 0l267.3 267.3c24.5 24.5 24.5 64.6 0 89.1-24.5 24.4-64.6 24.4-89.1-.1z'></path>
+            <path fill='#B6C3D2'
+                  d='M690.2 556.5L422.9 823.8c-24.5 24.5-64.6 24.5-89.1 0s-24.5-64.6 0-89.1l267.3-267.3c24.5-24.5 64.6-24.5 89.1 0 24.5 24.6 24.5 64.6 0 89.1z'></path>
+          </svg>
+        </a><a href='#/zh-CN/image' class='demo-home-nav__block'>
+          Image 图片
+          <svg viewBox='0 0 1024 1024' class='demo-home-nav__icon'>
+            <path fill='#B6C3D2'
+                  d='M601.1 556.5L333.8 289.3c-24.5-24.5-24.5-64.6 0-89.1s64.6-24.5 89.1 0l267.3 267.3c24.5 24.5 24.5 64.6 0 89.1-24.5 24.4-64.6 24.4-89.1-.1z'></path>
+            <path fill='#B6C3D2'
+                  d='M690.2 556.5L422.9 823.8c-24.5 24.5-64.6 24.5-89.1 0s-24.5-64.6 0-89.1l267.3-267.3c24.5-24.5 64.6-24.5 89.1 0 24.5 24.6 24.5 64.6 0 89.1z'></path>
+          </svg>
+        </a><a href='#/zh-CN/col' class='demo-home-nav__block'>
+          Layout 布局
+          <svg viewBox='0 0 1024 1024' class='demo-home-nav__icon'>
+            <path fill='#B6C3D2'
+                  d='M601.1 556.5L333.8 289.3c-24.5-24.5-24.5-64.6 0-89.1s64.6-24.5 89.1 0l267.3 267.3c24.5 24.5 24.5 64.6 0 89.1-24.5 24.4-64.6 24.4-89.1-.1z'></path>
+            <path fill='#B6C3D2'
+                  d='M690.2 556.5L422.9 823.8c-24.5 24.5-64.6 24.5-89.1 0s-24.5-64.6 0-89.1l267.3-267.3c24.5-24.5 64.6-24.5 89.1 0 24.5 24.6 24.5 64.6 0 89.1z'></path>
+          </svg>
+        </a><a href='#/zh-CN/popup' class='demo-home-nav__block'>
+          Popup 弹出层
+          <svg viewBox='0 0 1024 1024' class='demo-home-nav__icon'>
+            <path fill='#B6C3D2'
+                  d='M601.1 556.5L333.8 289.3c-24.5-24.5-24.5-64.6 0-89.1s64.6-24.5 89.1 0l267.3 267.3c24.5 24.5 24.5 64.6 0 89.1-24.5 24.4-64.6 24.4-89.1-.1z'></path>
+            <path fill='#B6C3D2'
+                  d='M690.2 556.5L422.9 823.8c-24.5 24.5-64.6 24.5-89.1 0s-24.5-64.6 0-89.1l267.3-267.3c24.5-24.5 64.6-24.5 89.1 0 24.5 24.6 24.5 64.6 0 89.1z'></path>
+          </svg>
+        </a><a href='#/zh-CN/style' class='demo-home-nav__block'>
+          Style 内置样式
+          <svg viewBox='0 0 1024 1024' class='demo-home-nav__icon'>
+            <path fill='#B6C3D2'
+                  d='M601.1 556.5L333.8 289.3c-24.5-24.5-24.5-64.6 0-89.1s64.6-24.5 89.1 0l267.3 267.3c24.5 24.5 24.5 64.6 0 89.1-24.5 24.4-64.6 24.4-89.1-.1z'></path>
+            <path fill='#B6C3D2'
+                  d='M690.2 556.5L422.9 823.8c-24.5 24.5-64.6 24.5-89.1 0s-24.5-64.6 0-89.1l267.3-267.3c24.5-24.5 64.6-24.5 89.1 0 24.5 24.6 24.5 64.6 0 89.1z'></path>
+          </svg>
+        </a><a href='#/zh-CN/toast' class='demo-home-nav__block'>
+          Toast 轻提示
+          <svg viewBox='0 0 1024 1024' class='demo-home-nav__icon'>
+            <path fill='#B6C3D2'
+                  d='M601.1 556.5L333.8 289.3c-24.5-24.5-24.5-64.6 0-89.1s64.6-24.5 89.1 0l267.3 267.3c24.5 24.5 24.5 64.6 0 89.1-24.5 24.4-64.6 24.4-89.1-.1z'></path>
+            <path fill='#B6C3D2'
+                  d='M690.2 556.5L422.9 823.8c-24.5 24.5-64.6 24.5-89.1 0s-24.5-64.6 0-89.1l267.3-267.3c24.5-24.5 64.6-24.5 89.1 0 24.5 24.6 24.5 64.6 0 89.1z'></path>
+          </svg>
+        </a></div>
+      </div>
+
+      <div class='demo-home-nav'>
+        <div class='demo-home-nav__title'>表单组件</div>
+        <div class='demo-home-nav__group'><a href='#/zh-CN/calendar' class='demo-home-nav__block'>
+          Calendar 日历
+          <svg viewBox='0 0 1024 1024' class='demo-home-nav__icon'>
+            <path fill='#B6C3D2'
+                  d='M601.1 556.5L333.8 289.3c-24.5-24.5-24.5-64.6 0-89.1s64.6-24.5 89.1 0l267.3 267.3c24.5 24.5 24.5 64.6 0 89.1-24.5 24.4-64.6 24.4-89.1-.1z'></path>
+            <path fill='#B6C3D2'
+                  d='M690.2 556.5L422.9 823.8c-24.5 24.5-64.6 24.5-89.1 0s-24.5-64.6 0-89.1l267.3-267.3c24.5-24.5 64.6-24.5 89.1 0 24.5 24.6 24.5 64.6 0 89.1z'></path>
+          </svg>
+        </a><a href='#/zh-CN/cascader' class='demo-home-nav__block hide-in-weapp'>
+          Cascader 级联选择
+          <svg viewBox='0 0 1024 1024' class='demo-home-nav__icon'>
+            <path fill='#B6C3D2'
+                  d='M601.1 556.5L333.8 289.3c-24.5-24.5-24.5-64.6 0-89.1s64.6-24.5 89.1 0l267.3 267.3c24.5 24.5 24.5 64.6 0 89.1-24.5 24.4-64.6 24.4-89.1-.1z'></path>
+            <path fill='#B6C3D2'
+                  d='M690.2 556.5L422.9 823.8c-24.5 24.5-64.6 24.5-89.1 0s-24.5-64.6 0-89.1l267.3-267.3c24.5-24.5 64.6-24.5 89.1 0 24.5 24.6 24.5 64.6 0 89.1z'></path>
+          </svg>
+        </a><a href='#/zh-CN/checkbox' class='demo-home-nav__block'>
+          Checkbox 复选框
+          <svg viewBox='0 0 1024 1024' class='demo-home-nav__icon'>
+            <path fill='#B6C3D2'
+                  d='M601.1 556.5L333.8 289.3c-24.5-24.5-24.5-64.6 0-89.1s64.6-24.5 89.1 0l267.3 267.3c24.5 24.5 24.5 64.6 0 89.1-24.5 24.4-64.6 24.4-89.1-.1z'></path>
+            <path fill='#B6C3D2'
+                  d='M690.2 556.5L422.9 823.8c-24.5 24.5-64.6 24.5-89.1 0s-24.5-64.6 0-89.1l267.3-267.3c24.5-24.5 64.6-24.5 89.1 0 24.5 24.6 24.5 64.6 0 89.1z'></path>
+          </svg>
+        </a><a href='#/zh-CN/datetime-picker' class='demo-home-nav__block'>
+          DatetimePicker 时间选择
+          <svg viewBox='0 0 1024 1024' class='demo-home-nav__icon'>
+            <path fill='#B6C3D2'
+                  d='M601.1 556.5L333.8 289.3c-24.5-24.5-24.5-64.6 0-89.1s64.6-24.5 89.1 0l267.3 267.3c24.5 24.5 24.5 64.6 0 89.1-24.5 24.4-64.6 24.4-89.1-.1z'></path>
+            <path fill='#B6C3D2'
+                  d='M690.2 556.5L422.9 823.8c-24.5 24.5-64.6 24.5-89.1 0s-24.5-64.6 0-89.1l267.3-267.3c24.5-24.5 64.6-24.5 89.1 0 24.5 24.6 24.5 64.6 0 89.1z'></path>
+          </svg>
+        </a><a href='#/zh-CN/field' class='demo-home-nav__block'>
+          Field 输入框
+          <svg viewBox='0 0 1024 1024' class='demo-home-nav__icon'>
+            <path fill='#B6C3D2'
+                  d='M601.1 556.5L333.8 289.3c-24.5-24.5-24.5-64.6 0-89.1s64.6-24.5 89.1 0l267.3 267.3c24.5 24.5 24.5 64.6 0 89.1-24.5 24.4-64.6 24.4-89.1-.1z'></path>
+            <path fill='#B6C3D2'
+                  d='M690.2 556.5L422.9 823.8c-24.5 24.5-64.6 24.5-89.1 0s-24.5-64.6 0-89.1l267.3-267.3c24.5-24.5 64.6-24.5 89.1 0 24.5 24.6 24.5 64.6 0 89.1z'></path>
+          </svg>
+        </a><a href='#/zh-CN/form' class='demo-home-nav__block hide-in-weapp'>
+          Form 表单
+          <svg viewBox='0 0 1024 1024' class='demo-home-nav__icon'>
+            <path fill='#B6C3D2'
+                  d='M601.1 556.5L333.8 289.3c-24.5-24.5-24.5-64.6 0-89.1s64.6-24.5 89.1 0l267.3 267.3c24.5 24.5 24.5 64.6 0 89.1-24.5 24.4-64.6 24.4-89.1-.1z'></path>
+            <path fill='#B6C3D2'
+                  d='M690.2 556.5L422.9 823.8c-24.5 24.5-64.6 24.5-89.1 0s-24.5-64.6 0-89.1l267.3-267.3c24.5-24.5 64.6-24.5 89.1 0 24.5 24.6 24.5 64.6 0 89.1z'></path>
+          </svg>
+        </a><a href='#/zh-CN/number-keyboard' class='demo-home-nav__block hide-in-weapp'>
+          NumberKeyboard 数字键盘
+          <svg viewBox='0 0 1024 1024' class='demo-home-nav__icon'>
+            <path fill='#B6C3D2'
+                  d='M601.1 556.5L333.8 289.3c-24.5-24.5-24.5-64.6 0-89.1s64.6-24.5 89.1 0l267.3 267.3c24.5 24.5 24.5 64.6 0 89.1-24.5 24.4-64.6 24.4-89.1-.1z'></path>
+            <path fill='#B6C3D2'
+                  d='M690.2 556.5L422.9 823.8c-24.5 24.5-64.6 24.5-89.1 0s-24.5-64.6 0-89.1l267.3-267.3c24.5-24.5 64.6-24.5 89.1 0 24.5 24.6 24.5 64.6 0 89.1z'></path>
+          </svg>
+        </a><a href='#/zh-CN/password-input' class='demo-home-nav__block hide-in-weapp'>
+          PasswordInput 密码输入框
+          <svg viewBox='0 0 1024 1024' class='demo-home-nav__icon'>
+            <path fill='#B6C3D2'
+                  d='M601.1 556.5L333.8 289.3c-24.5-24.5-24.5-64.6 0-89.1s64.6-24.5 89.1 0l267.3 267.3c24.5 24.5 24.5 64.6 0 89.1-24.5 24.4-64.6 24.4-89.1-.1z'></path>
+            <path fill='#B6C3D2'
+                  d='M690.2 556.5L422.9 823.8c-24.5 24.5-64.6 24.5-89.1 0s-24.5-64.6 0-89.1l267.3-267.3c24.5-24.5 64.6-24.5 89.1 0 24.5 24.6 24.5 64.6 0 89.1z'></path>
+          </svg>
+        </a><a href='#/zh-CN/picker' class='demo-home-nav__block'>
+          Picker 选择器
+          <svg viewBox='0 0 1024 1024' class='demo-home-nav__icon'>
+            <path fill='#B6C3D2'
+                  d='M601.1 556.5L333.8 289.3c-24.5-24.5-24.5-64.6 0-89.1s64.6-24.5 89.1 0l267.3 267.3c24.5 24.5 24.5 64.6 0 89.1-24.5 24.4-64.6 24.4-89.1-.1z'></path>
+            <path fill='#B6C3D2'
+                  d='M690.2 556.5L422.9 823.8c-24.5 24.5-64.6 24.5-89.1 0s-24.5-64.6 0-89.1l267.3-267.3c24.5-24.5 64.6-24.5 89.1 0 24.5 24.6 24.5 64.6 0 89.1z'></path>
+          </svg>
+        </a><a href='#/zh-CN/radio' class='demo-home-nav__block'>
+          Radio 单选框
+          <svg viewBox='0 0 1024 1024' class='demo-home-nav__icon'>
+            <path fill='#B6C3D2'
+                  d='M601.1 556.5L333.8 289.3c-24.5-24.5-24.5-64.6 0-89.1s64.6-24.5 89.1 0l267.3 267.3c24.5 24.5 24.5 64.6 0 89.1-24.5 24.4-64.6 24.4-89.1-.1z'></path>
+            <path fill='#B6C3D2'
+                  d='M690.2 556.5L422.9 823.8c-24.5 24.5-64.6 24.5-89.1 0s-24.5-64.6 0-89.1l267.3-267.3c24.5-24.5 64.6-24.5 89.1 0 24.5 24.6 24.5 64.6 0 89.1z'></path>
+          </svg>
+        </a><a href='#/zh-CN/rate' class='demo-home-nav__block'>
+          Rate 评分
+          <svg viewBox='0 0 1024 1024' class='demo-home-nav__icon'>
+            <path fill='#B6C3D2'
+                  d='M601.1 556.5L333.8 289.3c-24.5-24.5-24.5-64.6 0-89.1s64.6-24.5 89.1 0l267.3 267.3c24.5 24.5 24.5 64.6 0 89.1-24.5 24.4-64.6 24.4-89.1-.1z'></path>
+            <path fill='#B6C3D2'
+                  d='M690.2 556.5L422.9 823.8c-24.5 24.5-64.6 24.5-89.1 0s-24.5-64.6 0-89.1l267.3-267.3c24.5-24.5 64.6-24.5 89.1 0 24.5 24.6 24.5 64.6 0 89.1z'></path>
+          </svg>
+        </a><a href='#/zh-CN/search' class='demo-home-nav__block'>
+          Search 搜索
+          <svg viewBox='0 0 1024 1024' class='demo-home-nav__icon'>
+            <path fill='#B6C3D2'
+                  d='M601.1 556.5L333.8 289.3c-24.5-24.5-24.5-64.6 0-89.1s64.6-24.5 89.1 0l267.3 267.3c24.5 24.5 24.5 64.6 0 89.1-24.5 24.4-64.6 24.4-89.1-.1z'></path>
+            <path fill='#B6C3D2'
+                  d='M690.2 556.5L422.9 823.8c-24.5 24.5-64.6 24.5-89.1 0s-24.5-64.6 0-89.1l267.3-267.3c24.5-24.5 64.6-24.5 89.1 0 24.5 24.6 24.5 64.6 0 89.1z'></path>
+          </svg>
+        </a><a href='#/zh-CN/slider' class='demo-home-nav__block'>
+          Slider 滑块
+          <svg viewBox='0 0 1024 1024' class='demo-home-nav__icon'>
+            <path fill='#B6C3D2'
+                  d='M601.1 556.5L333.8 289.3c-24.5-24.5-24.5-64.6 0-89.1s64.6-24.5 89.1 0l267.3 267.3c24.5 24.5 24.5 64.6 0 89.1-24.5 24.4-64.6 24.4-89.1-.1z'></path>
+            <path fill='#B6C3D2'
+                  d='M690.2 556.5L422.9 823.8c-24.5 24.5-64.6 24.5-89.1 0s-24.5-64.6 0-89.1l267.3-267.3c24.5-24.5 64.6-24.5 89.1 0 24.5 24.6 24.5 64.6 0 89.1z'></path>
+          </svg>
+        </a><a href='#/zh-CN/stepper' class='demo-home-nav__block'>
+          Stepper 步进器
+          <svg viewBox='0 0 1024 1024' class='demo-home-nav__icon'>
+            <path fill='#B6C3D2'
+                  d='M601.1 556.5L333.8 289.3c-24.5-24.5-24.5-64.6 0-89.1s64.6-24.5 89.1 0l267.3 267.3c24.5 24.5 24.5 64.6 0 89.1-24.5 24.4-64.6 24.4-89.1-.1z'></path>
+            <path fill='#B6C3D2'
+                  d='M690.2 556.5L422.9 823.8c-24.5 24.5-64.6 24.5-89.1 0s-24.5-64.6 0-89.1l267.3-267.3c24.5-24.5 64.6-24.5 89.1 0 24.5 24.6 24.5 64.6 0 89.1z'></path>
+          </svg>
+        </a><a href='#/zh-CN/switch' class='demo-home-nav__block'>
+          Switch 开关
+          <svg viewBox='0 0 1024 1024' class='demo-home-nav__icon'>
+            <path fill='#B6C3D2'
+                  d='M601.1 556.5L333.8 289.3c-24.5-24.5-24.5-64.6 0-89.1s64.6-24.5 89.1 0l267.3 267.3c24.5 24.5 24.5 64.6 0 89.1-24.5 24.4-64.6 24.4-89.1-.1z'></path>
+            <path fill='#B6C3D2'
+                  d='M690.2 556.5L422.9 823.8c-24.5 24.5-64.6 24.5-89.1 0s-24.5-64.6 0-89.1l267.3-267.3c24.5-24.5 64.6-24.5 89.1 0 24.5 24.6 24.5 64.6 0 89.1z'></path>
+          </svg>
+        </a><a href='#/zh-CN/uploader' class='demo-home-nav__block'>
+          Uploader 文件上传
+          <svg viewBox='0 0 1024 1024' class='demo-home-nav__icon'>
+            <path fill='#B6C3D2'
+                  d='M601.1 556.5L333.8 289.3c-24.5-24.5-24.5-64.6 0-89.1s64.6-24.5 89.1 0l267.3 267.3c24.5 24.5 24.5 64.6 0 89.1-24.5 24.4-64.6 24.4-89.1-.1z'></path>
+            <path fill='#B6C3D2'
+                  d='M690.2 556.5L422.9 823.8c-24.5 24.5-64.6 24.5-89.1 0s-24.5-64.6 0-89.1l267.3-267.3c24.5-24.5 64.6-24.5 89.1 0 24.5 24.6 24.5 64.6 0 89.1z'></path>
+          </svg>
+        </a></div>
+      </div>
+
+
+    </section>
+  </div>
+</template>
+
+<style lang='scss' scoped>
+.simulator {
+  box-sizing: border-box;
+  width: 100%;
+  height: 80vh;
+  padding: 46px 20px 20px;
+  background: #fff;
+  overflow-y: auto;
+  overflow-x: hidden;
+
+  header {
+    display: none;
+  }
+
+  .demo-home__title {
+    margin: 0 0 16px;
+    font-size: 32px;
+
+    img {
+      width: 32px;
+    }
+  }
+
+  .demo-home__desc {
+    margin: 0 0 40px;
+    color: rgba(69, 90, 100, 0.6);
+    font-size: 14px;
+  }
+
+  .demo-home-nav__block {
+    position: relative;
+    display: -webkit-box;
+    display: -webkit-flex;
+    display: flex;
+    margin: 0 0 12px;
+    padding-left: 20px;
+    color: #323233;
+    font-weight: 600;
+    font-size: 14px;
+    line-height: 40px;
+    background: #f7f8fa;
+    border-radius: 99px;
+    -webkit-transition: background 0.3s;
+    transition: background 0.3s;
+  }
+
+  .demo-home-nav__icon {
+    position: absolute;
+    top: 50%;
+    right: 16px;
+    width: 16px;
+    height: 16px;
+    margin-top: -8px;
+  }
+
+}
+
+.simulator::-webkit-scrollbar {
+  width: 0px;
+  /*height: 4px;*/
+}
+
+.simulator::-webkit-scrollbar-thumb {
+  border-radius: 10px;
+  -webkit-box-shadow: inset 0 0 5px rgba(0, 0, 0, 0.2);
+  background: rgba(0, 0, 0, 0.2);
+}
+
+.simulator::-webkit-scrollbar-track {
+  -webkit-box-shadow: inset 0 0 5px rgba(0, 0, 0, 0.2);
+  border-radius: 0;
+  background: rgba(0, 0, 0, 0.1);
+
+}
+</style>
